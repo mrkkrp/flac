c4624cc445a7d5b8d68a60a1d7ff89848251fbc9
mrkkrp <markkarpov@opmbx.org>
2016-12-23 00:04:41 +0300

    Use ‘Builder’ for construction of apodization spec

--- Codec/Audio/FLAC/StreamEncoder/Internal/Helpers.hs
+++ Codec/Audio/FLAC/StreamEncoder/Internal/Helpers.hs
@@ -19,13 +19,15 @@ where
 
 import Codec.Audio.FLAC.StreamEncoder.Internal.Types
 import Data.ByteString (ByteString)
+import Data.List (intersperse)
 import Data.List.NonEmpty (NonEmpty (..))
 import Data.Monoid ((<>))
 import Data.Word (Word64)
 import Foreign.C.String
-import qualified Data.ByteString       as B
-import qualified Data.ByteString.Char8 as B8
-import qualified Data.List.NonEmpty    as NE
+import Numeric.Natural
+import qualified Data.ByteString.Builder as BU
+import qualified Data.ByteString.Lazy    as BL
+import qualified Data.List.NonEmpty      as NE
 
 -- | Encode given input file, return 'False' in case of failure.
 
@@ -50,34 +52,41 @@ foreign import ccall unsafe "FLAC__stream_encoder_process_helper"
 -- <https://xiph.org/flac/api/group__flac__stream__encoder.html#ga6598f09ac782a1f2a5743ddf247c81c8>.
 
 renderApodizationSpec :: NonEmpty ApodizationFunction -> ByteString
-renderApodizationSpec = B.intercalate ";" . NE.toList . fmap f
+renderApodizationSpec =
+  BL.toStrict         .
+  BU.toLazyByteString .
+  mconcat             .
+  intersperse ";"     .
+  NE.toList           .
+  fmap f
   where
+    f :: ApodizationFunction -> BU.Builder
     f Bartlett                = "bartlett"
     f BartlettHann            = "bartlett_hann"
     f Blackman                = "blackman"
     f BlackmanHarris4Term92Db = "blackman_harris_4term_92db"
     f Connes                  = "connes"
     f Flattop                 = "flattop"
-    f (Gauss stddev)          = "gauss(" <> showbs stddev <> ")"
+    f (Gauss stddev)          = "gauss(" <> BU.doubleDec stddev <> ")"
     f Hamming                 = "hamming"
     f Hann                    = "hann"
     f KaiserBessel            = "kaiser_bessel"
     f Nuttall                 = "nuttall"
     f Rectangle               = "rectangle"
     f Triangle                = "triangle"
-    f (Tukey p)               = "tukey(" <> showbs p <> ")"
-    f (PartialTukey n Nothing)
-      = "partial_tukey(" <> showbs n <> ")"
-    f (PartialTukey n (Just (ov, Nothing)))
-      = "partial_tukey(" <> showbs n <> "/" <> showbs ov <> ")"
-    f (PartialTukey n (Just (ov, Just p)))
-      = "partial_tukey(" <> showbs n <> "/" <> showbs ov <> "/" <> showbs p <> ")"
-    f (PunchoutTukey n Nothing)
-      = "punchout_tukey(" <> showbs n <> ")"
-    f (PunchoutTukey n (Just (ov, Nothing)))
-      = "punchout_tukey(" <> showbs n <> "/" <> showbs ov <> ")"
-    f (PunchoutTukey n (Just (ov, Just p)))
-      = "punchout_tukey(" <> showbs n <> "/" <> showbs ov <> "/" <> showbs p <> ")"
+    f (Tukey p)               = "tukey(" <> BU.doubleDec p <> ")"
+    f (PartialTukey n Nothing) =
+      "partial_tukey(" <> natDec n <> ")"
+    f (PartialTukey n (Just (ov, Nothing))) =
+      "partial_tukey(" <> natDec n <> "/" <> BU.doubleDec ov <> ")"
+    f (PartialTukey n (Just (ov, Just p))) =
+      "partial_tukey(" <> natDec n <> "/" <> BU.doubleDec ov <> "/" <> BU.doubleDec p <> ")"
+    f (PunchoutTukey n Nothing) =
+      "punchout_tukey(" <> natDec n <> ")"
+    f (PunchoutTukey n (Just (ov, Nothing))) =
+      "punchout_tukey(" <> natDec n <> "/" <> BU.doubleDec ov <> ")"
+    f (PunchoutTukey n (Just (ov, Just p))) =
+      "punchout_tukey(" <> natDec n <> "/" <> BU.doubleDec ov <> "/" <> BU.doubleDec p <> ")"
     f Welch                   = "welch"
-    showbs :: Show a => a -> ByteString
-    showbs = B8.pack . show
+    natDec :: Natural -> BU.Builder
+    natDec = BU.integerDec . fromIntegral
