84ded3084eedd04eb6c42ab38cc37b4d29cbd4be
mrkkrp <markkarpov92@gmail.com>
2017-12-27 14:30:15 +0700

    Add a missing article
    [skip ci]

--- Codec/Audio/FLAC/Metadata.hs
+++ Codec/Audio/FLAC/Metadata.hs
@@ -282,7 +282,7 @@ class MetaValue a where
   -- | Given a value that determines what to write and a value to write,
   -- add\/replace a piece of metadata information. This is how you edit
   -- metadata. To delete something, set it to 'Nothing' (well, it should be
-  -- something that /can be missing/, for example you cannot delete
+  -- something that /can be missing/, for example you cannot delete the
   -- 'SampleRate' attribute). If 'MetaWritable' is defined, this method must
   -- be defined as well.
 
