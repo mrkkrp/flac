e689f370f37ec718843756630ab223b2deed2bdf
mrkkrp <markkarpov92@gmail.com>
2019-01-01 13:12:12 +0700

    Update copyright years
    [skip ci]

--- Codec/Audio/FLAC/Util.hs
+++ Codec/Audio/FLAC/Util.hs
@@ -1,6 +1,6 @@
 -- |
 -- Module      :  Codec.Audio.FLAC.Util
--- Copyright   :  © 2016–2018 Mark Karpov
+-- Copyright   :  © 2016–2019 Mark Karpov
 -- License     :  BSD 3 clause
 --
 -- Maintainer  :  Mark Karpov <markkarpov92@gmail.com>
